57bb1ebb61412d051b171cbe9cef351c6af7ef64
shiwangi-07 <shiwangi1204@gmail.com>
2025-04-03 02:01:15 +0530

    added features in dashboard

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
+import DashboardPage from "./page";
+import { BarLoader } from "react-spinners";
 
-const MainLayout = ({ children }) => {
-  return <div className="container mx-auto my-32">{children}</div>;
+const DashboardLayout = () => {
+  return (
+    <div className="px-5">
+      <h1 className="text-6xl font-bold gradient-title mb-5">Dashboard</h1>
+
+      {/* Dashboard Page */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <DashboardPage />
+      </Suspense>
+    </div>
+  );
 };
 
-export default MainLayout;
+export default DashboardLayout;
