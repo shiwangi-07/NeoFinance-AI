3319137d9d9a5fe69d82c2be022d7e57dc1b6dd2
shiwangi-07 <shiwangi1204@gmail.com>
2025-04-14 02:23:37 +0530

    added monthly budget feature

---
+++ app/(main)/dashboard/_components/budget-progress.jsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Check, Pencil, X } from "lucide-react";
+import React, { useState } from "react";
+
+const BudgetProgress = ({ initialBudget, currentExpenses }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newBudget, setNewBudget] = useState(
+    initialBudget?.amount?.toString() || ""
+  );
+
+  const percentUsed = initialBudget
+    ? (currentExpenses / initialBudget.amount) * 100
+    : 0;
+
+  const handleUpdateBudget = () => {};
+
+  const handleCancel = () => {
+    setNewBudget(initialBudget?.amount?.toString() || "");
+    setIsEditing(false);
+  };
+
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <div className="flex-1">
+          <CardTitle>Monthly Budget (Default Account)</CardTitle>
+          <div className="flex items-center gap-2 mt-1">
+            {isEditing ? (
+              <div className="flex items-center gap-2">
+                <Input
+                  type="number"
+                  value={newBudget}
+                  onChange={(e) => setNewBudget(e.target.value)}
+                  className="w-32"
+                  placeholder="Enter amount"
+                  autoFocus
+                />
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={handleUpdateBudget}
+                >
+                  <Check className="h-4 w-4 text-green-500" />
+                </Button>
+                <Button variant="ghost" size="icon" onClick={handleCancel}>
+                  <X className="h-4 w-4 text-red-500" />
+                </Button>
+              </div>
+            ) : (
+              <>
+                <CardDescription>
+                  {initialBudget
+                    ? `$${currentExpenses.toFixed(
+                        2
+                      )} of $${initialBudget.amount.toFixed(2)} spent`
+                    : "No budget set"}
+                </CardDescription>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => setIsEditing(true)}
+                  className={"h-6 w-3"}
+                >
+                  <Pencil className="h-3 w-3" />
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default BudgetProgress;
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -3,13 +3,28 @@ import CreateAccountDrawer from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import AccountCard from "./_components/account-card";
+import { getCurrentBudget } from "@/actions/budget";
+import BudgetProgress from "./_components/budget-progress";
 
 async function DashboardPage() {
   const accounts = await getUserAccounts();
 
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
+
   return (
     <div className="px-5">
       {/* Budget Progress */}
+      {defaultAccount && (
+        <BudgetProgress
+          initialBudget={budgetData?.budget}
+          currentExpenses={budgetData?.currentExpenses || 0}
+        />
+      )}
 
       {/* Dashboard Overview */}
 
